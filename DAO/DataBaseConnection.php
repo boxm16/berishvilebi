8912e9f7b236df0f034903ac453a7e6c97467a27
unknown <boxm16@yahoo.com>
2021-05-10 02:53:51 +0300

    DataBaseConnection ajusted to server (local, web)

--- DAO/DataBaseConnection.php
+++ DAO/DataBaseConnection.php
@@ -4,35 +4,24 @@ require_once 'mPDO.php';
 
 class DataBaseConnection {
 
-    public function getPDO() {
+    private $db;
+    private $user;
 
-
-
-        $host = 'remotemysql.com';
-        $db = '9w706j5s1P';
-        $user = '9w706j5s1P';
-        $pass = 'zEcc9jTAyc';
-        $charset = 'utf8';
-
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ];
-        try {
-            $pdo = new mPDO($dsn, $user, $pass, $options);
-            return $pdo;
-        } catch (\PDOException $e) {
-            echo $e->getMessage() . " Eroor Code:";
-            echo $e->getCode();
+    function __construct() {
+        $server = $_SERVER['SERVER_NAME'];
+        if ($server == "localhost") {
+            $this->db = '231185';
+            $this->user = '231185';
+        } else {
+            $this->db = '231185';
+            $this->user = '231185';
         }
     }
 
     public function getLocalhostConnection() {
         $host = 'localhost';
-        $db = '231185';
-        $user = '231185';
+        $db = $this->db;
+        $user = $this->user;
         $pass = 'athina2004';
         $charset = 'utf8';
 

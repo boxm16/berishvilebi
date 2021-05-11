9e1c72867fdcc2b3cfedaba36f489f6b11eab5c7
unknown <boxm16@yahoo.com>
2021-05-12 00:36:03 +0300

    quick adds

--- cronJobDispatcher.php
+++ cronJobDispatcher.php
@@ -2,7 +2,7 @@
 
 require_once 'Controller/CronJobController.php';
 require_once 'Controller/ReportController.php';
-
+$s = microtime(true);
 $cronJobController = new CronJobController();
 if (isset($_GET["uploadingStatusRequest"])) {//this request comes from index.php ajax every second
     if ($cronJobController->getUploadingStatus()) {
@@ -14,15 +14,17 @@ if (isset($_GET["uploadingStatusRequest"])) {//this request comes from index.php
     if ($cronJobController->getUploadingStatus()) {
         echo "uploading...<br>";
         $cronJobController->insertNextChunk();
-    } else
-        $ids = $cronJobController->getRouteDetailsReportIds();
-    if (count($ids) > 0) {
-        echo "creating Route Details Report...<br>";
-        $reportController = new ReportController();
-        $reportId = $ids[0];
-        $reportController->createRouteDetailsReport($reportId);
     } else {
-        echo "ready<br>";
+        $ids = $cronJobController->getRouteDetailsReportIds();
+        if (count($ids) > 0) {
+            echo "creating Route Details Report...<br>";
+            $reportController = new ReportController();
+            $reportId = $ids[0];
+            $reportController->createRouteDetailsReport($reportId);
+        } else {
+            echo "ready<br>";
+        }
     }
 }
-?>
+$e = microtime(true);
+echo "<br> Display time required:" . ($e - $s);

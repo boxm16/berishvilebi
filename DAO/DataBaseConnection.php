0759feeb59b1f5da36a524939f09bd39634322a7
unknown <boxm16@yahoo.com>
2021-05-13 00:40:01 +0300

    quick commit

--- DAO/DataBaseConnection.php
+++ DAO/DataBaseConnection.php
@@ -6,15 +6,22 @@ class DataBaseConnection {
 
     private $db;
     private $user;
+    private $pass;
 
     function __construct() {
         $server = $_SERVER['SERVER_NAME'];
         if ($server == "localhost") {
             $this->db = '231185';
             $this->user = '231185';
+            $this->pass = 'athina2004';
         } else {
-            $this->db = '257831';
-            $this->user = '257831';
+            //$this->db = '257831';
+            //$this->user = '257831';
+           // $this->db = 'athina2004';
+            
+            $this->db = 'id16806060_berishvilidb';
+            $this->user = 'id16806060_berishvili';
+            $this->pass = 'RI&ETCNC^CR5=Fl+';
         }
     }
 
@@ -22,7 +29,7 @@ class DataBaseConnection {
         $host = 'localhost';
         $db = $this->db;
         $user = $this->user;
-        $pass = 'athina2004';
+        $pass = $this->pass;
         $charset = 'utf8';
 
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";

6ae690dc5eb7aaf55b1a3fdfb8fa6cc3fb138815
unknown <boxm16@yahoo.com>
2021-05-14 11:36:53 +0300

    remote database added

--- DAO/DataBaseConnection.php
+++ DAO/DataBaseConnection.php
@@ -17,19 +17,41 @@ class DataBaseConnection {
         } else {
             //$this->db = '257831';
             //$this->user = '257831';
-           // $this->db = 'athina2004';
-            
+            // $this->db = 'athina2004';
+
             $this->db = 'id16806060_berishvilidb';
             $this->user = 'id16806060_berishvili';
             $this->pass = 'RI&ETCNC^CR5=Fl+';
         }
     }
 
-    public function getLocalhostConnection() {
+    public function getLocalhostConnection_X() {
         $host = 'localhost';
         $db = $this->db;
         $user = $this->user;
         $pass = $this->pass;
+        $charset = 'utf8';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        try {
+            $pdo = new mPDO($dsn, $user, $pass, $options);
+            return $pdo;
+        } catch (\PDOException $e) {
+            echo $e->getMessage() . " Error Code:";
+            echo $e->getCode();
+        }
+    }
+
+    public function getLocalhostConnection() {
+        $host = "remotemysql.com";
+        $db = "C1Vlp1x4Mf";
+        $user = "C1Vlp1x4Mf";
+        $pass = "9hXW8TtqWH";
         $charset = 'utf8';
 
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";

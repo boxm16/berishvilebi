4e14c36dae70cc26c41a3b9ec32466de7f9f454b
unknown <boxm16@yahoo.com>
2022-04-07 19:12:25 +0300

    upload commit

--- admin.php
+++ admin.php
@@ -66,7 +66,7 @@ if (isset($_GET["personInFocusId"])) {
             <hr>
             <?php
             ?>
-            <svg style="background-color:skyblue" width="100%"  height="<?php echo $svgHeight; ?>">
+            <svg style="background-color:skyblue" width="100%"  height="7000">
 
             <?php
             foreach ($personsList as $person) {

d6081b280f7004d813e66b8e75cd993553df539d
unknown <boxm16@yahoo.com>
2022-04-15 22:21:20 +0300

    databaseConnection added:

--- DAO/ConfigDao.php
+++ DAO/ConfigDao.php
@@ -1,8 +1,18 @@
 <?php
 
+require_once 'DataBaseConnection.php';
+
 class ConfigDao {
 
+    private $connection;
+
+    function __construct() {
+        $dbConnection = new DataBaseConnection();
+        $this->connection = $dbConnection->getConnection();
+    }
+
     function createTables() {
+
         echo "labu";
     }
 

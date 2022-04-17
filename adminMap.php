447bfe95d5bf152d29625364d80287fe9cdc7096
unknown <boxm16@yahoo.com>
2022-04-17 16:45:05 +0300

    adminMap added

---
+++ adminMap.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title></title>
+        <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
+        <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <style>
+
+        </style>
+    </head>
+    <body style="width:<?php echo $width ?>px;">
+        <script>
+        </script>
+    </body>
+</html>
